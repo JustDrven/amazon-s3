6ae8e08960d13e979ba038e4581fd45daa58c39e
JustDrven <petr.nemec11111@gmail.com>
2026-06-19 13:32:07 +0200

    feat: create auth files, add enduser file path in config

--- cmd/configuration/model.go
+++ cmd/configuration/model.go
@@ -3,4 +3,5 @@ package configuration
 type ConfigData struct {
 	Port              int    `json:"port"`
 	StorageFolderPath string `json:"storageFolderPath"`
+	EndUserFilePath   string `json:"enduserFilePath"`
 }
